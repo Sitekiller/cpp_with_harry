bd722a9668761d295548b8e6d7412ca3ef1a6354
Sitekiller <nsuyash67@gmail.com>
2025-03-18 16:51:27 +0530

    bool

--- data_type.cpp
+++ data_type.cpp
@@ -5,6 +5,7 @@ int main(){
     int a=5;
     float b=1.5;
     char c='a';
-    cout<<"the valve of a is"<<a<<"\nthe value of b is "<<b<<"\nthe vailue of c is"<<c;
+    bool n =false;
+    cout<<"the valve of a is"<<a<<"\nthe value of b is "<<b<<"\nthe vailue of c is"<<c<<"\n the statement is "<<n;
     return 0;
 }
