74df2f28c9806a58f2deabd1f1030584e5788a78
Sitekiller <nsuyash67@gmail.com>
2025-03-18 17:03:16 +0530

    in put out

---
+++ in_out.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num1,num2;
+    cout<<"enter the number 1 ";
+    cin>>num1;
+    cout<<"enter the number2";
+    cin>>num2;
+    cout<<"the num 1 is "<<num1<<"\n the num2 is "<<num2;
+            return 0;
+}
