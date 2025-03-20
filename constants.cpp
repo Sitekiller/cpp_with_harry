1dbfde3dbd12bed844bf91ecab6202e19ace745c
SUYASH NIKAM <nsuyash67@gmail.com>
2025-03-20 15:46:55 +0530

    Update constants.cpp

--- constants.cpp
+++ constants.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
     const int a=3;
-   // a=45;
+    a=45;
     cout<<a;/*here using the const the valve of a is not change and the compiler show yhe error*/
     return 0;
 }
