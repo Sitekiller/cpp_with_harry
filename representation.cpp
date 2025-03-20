a61bb070e50249430e561a929097b1f284fcf41f
Sitekiller <nsuyash67@gmail.com>
2025-03-20 15:18:34 +0530

    multiple varible

---
+++ representation.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float a=34.14f;
+    long double b=34.14l;
+    cout<<"th value of float is" << a<<endl<<"the valve of long double is" << b;
+    return 0;
+}/*here the f and  l use after varible to deside the varible from reselt */
